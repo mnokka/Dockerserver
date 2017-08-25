818909f2ae5b7c93947084d053edb171c87892f9
Mika Nokka <mika.nokka1@gmail.com>
2017-08-25 15:35:12 +0300

    Answers /cat request

--- app/server.py
+++ app/server.py
@@ -2,10 +2,14 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
-
 def hello_world():
-	return "HELLOS\n"
+	return "HELLOS THERE\n"
+
+@app.route('/cat')
+def cat():
+    return "Cat says: MIU\n"
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
+		
 		
